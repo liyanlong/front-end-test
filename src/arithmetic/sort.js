33df35bebefda6c9aff571e6d0b0d55fb785c5c9
小小龙 <295697141@qq.com>
2019-09-22 10:10:10 +0800

    Update: sort.js
    fixed: bubble_sort

--- src/arithmetic/sort.js
+++ src/arithmetic/sort.js
@@ -3,16 +3,20 @@
 /**
  * 冒泡排序算法
  * 最优情况 O(n), 最差情况 O(n)^2
- * 
+ * example: [12, 0, 5, 3]
+ * 第一次外循环: [0, 12, 5, 3]
+ * 第二次外循环: [0, 3, 12, 5]
+ * 第三次外循环: [0, 3, 5, 12]
  * @param {Array} arr 带排序数组
  * @param {String} sort 排序规则 desc: 降序, asc： 升序
  * 
  * @return {Array} 已排序数组
  */
-export function bubble_sort (arr, sort = 'desc') {
+export function bubble_sort (arr, sort = 'asc') {
   arr = arr.slice()
-  for (let i = 0; i < arr.length; i ++) {
-    for (let j = 1; j < arr.length - i - i; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      // 最小值在第一位
       if (arr[j] > arr[j + 1]) {
         let tmp = arr[j + 1]
         arr[j + 1] = arr[j]
@@ -20,5 +24,5 @@ export function bubble_sort (arr, sort = 'desc') {
       }
     }
   }
-  return sort === 'desc' ? arr : arr.reverse()
+  return sort === 'asc' ? arr : arr.reverse()
 }
